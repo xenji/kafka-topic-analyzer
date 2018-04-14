fc159c5d9ea1b3bbcfca342abb1157c048121a84
Mario Mueller <mario@xenji.com>
2018-04-14 12:06:54 +0200

    Running the binary in CI is a bad idea when you have required cli params

--- ci/script.sh
+++ ci/script.sh
@@ -14,8 +14,9 @@ main() {
     cross test --target $TARGET
     cross test --target $TARGET --release
 
-    cross run --target $TARGET
-    cross run --target $TARGET --release
+    # Running it is a bad idea when you have required cli params
+    # cross run --target $TARGET
+    # cross run --target $TARGET --release
 }
 
 # we don't run the "test phase" when doing deploys
